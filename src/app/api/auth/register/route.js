0986498a8d0d675661ebed1fbae33c13b8d2753a
AgredaLuis <Luis.Agreda98@gmail.com>
2023-11-05 19:14:28 -0400

    Proyecto finalizado

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,65 @@
+import { NextResponse } from "next/server";
+import bcrypt from 'bcrypt'
+import bd from "@/libs/bd";
+
+export async function POST(request) {
+  try {
+    const data = await request.json();
+
+    const userFound = await bd.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (userFound) {
+      return NextResponse.json(
+        {
+          message: "Email already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const usernameFound = await bd.user.findUnique({
+      where: {
+        username: data.username,
+      },
+    });
+
+    if (usernameFound) {
+      return NextResponse.json(
+        {
+          message: "username already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const newUser = await bd.user.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+
+    const { password: _, ...user } = newUser;
+
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
